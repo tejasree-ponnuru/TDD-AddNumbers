a4b1dc715d15fc8d7daf37c38b8073d5d399acac
tejasree-ponnuru <tejasreeponnuru@gmail.com>
2025-09-08 22:00:12 +0530

    Handle different delimiter passed to add function

--- add.ts
+++ add.ts
@@ -1,7 +1,18 @@
 export const add = (nums: string): number =>{
     if(nums=="")return 0;
-    const delimiter: RegExp=/,|\n/
-    const arr=nums.split(delimiter).map(Number)
+    let delimiter: RegExp=/,|\n/;
+    const escapeRegExp = (s: string): string =>
+        s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    if(nums.startsWith("//")){
+        const match=nums.match(/^\/\/(.+)\n/);
+        if(!match){
+            throw Error("Invalid delimiter given");
+        }
+        const matchedDelimiter=escapeRegExp(match[1]);
+        delimiter=new RegExp(matchedDelimiter);
+        nums=nums.slice(match[0].length);
+    }
+    let arr: number[]=nums.trim().split(delimiter).map(Number);
     const sum: number=arr.reduce((acc,n)=>acc+=n,0)
     return sum
 }
